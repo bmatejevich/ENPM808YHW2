97871286dd0c88c24b0e2f1a9a0f29de1979e41a
Brian Matejevich <brianmatejevich@Brians-MacBook-Pro-7.local>
2020-06-25 15:15:24 -0400

    Added gitignore file

--- PytorchMain.py
+++ PytorchMain.py
@@ -8,16 +8,18 @@ import numpy as np
 import matplotlib as mpl
 from Problem1 import *
 from Problem2 import *
+import os
 
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 
 flag = False
 runboolean = True
 
 def mainprog(runboolean):
-    if __name__ == '__main__':  # if this is the main file in which all files are imported to 
-        problem = 2 # make sure to define the problem you want 1 or 2
-        
+    if __name__ == '__main__':  # if this is the main file in which all files are imported to
+        problem = 1 # make sure to define the problem you want 1 or 2
+
         if problem==1:
             Problem1Main()
         elif problem==2:
@@ -34,6 +36,3 @@ if __name__ == '__main__':
     runboolean = True
     while runboolean == True:
         runboolean = mainprog(runboolean)
-
-
-
